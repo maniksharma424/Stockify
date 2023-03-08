37bc221cb42e0723533f3695a3b2113f4a6b6c01
MANIKSHARMA2000 <115614705+MANIKSHARMA2000@users.noreply.github.com>
2023-03-08 23:53:43 +0530

    Api endpoints

--- ProjectDetails.js
+++ ProjectDetails.js
@@ -1,54 +1,19 @@
+//Add this file to gitIgnore before deployement
+
+
+
+
 //Meta Dveloper 
 // login-maniksharma424@gmail.com
 // pass-user1@1998
 
+//IMP LINKS
+
+//-------------All Api Endpoints--------------------
+//https://developers.facebook.com/docs/whatsapp/cloud-api/reference
 
 
-
-
-// Whatsapp Subscriber Post Request URL
-
-
-// const fetch = require('node-fetch');
-
-// const data = JSON.stringify({
-//   messaging_product: "whatsapp",
-//   to: "919682147830",
-//   type: "template",
-//   template: {
-//     name: "hello_world",
-//     language: {
-//       code: "en_US"
-//     }
-//   }
-// });
-
-// const options = {
-//   method: 'POST',
-//   headers: {
-//     'Authorization': 'Bearer EAAPiNh6MJLcBAA9ZC6qCiAhmQjZAFJXA1rEI1rnYFICLnKMW2yJKJZBQLRFwtFE0URqJcnvsUEfDI4jVWzGyXmsTQ3izzVtX4pBFmmSKgfDVh0fGsj76elmY7PaNz9BucmfAcsquO0ZArX941NlHsCWkhOJKat5SyaPJe14gwpY6fME4p47g0tPKYiJblkSAZBZADKC4oMSrxdkCRDrfZCu',
-//     'Content-Type': 'application/json',
-//     'Content-Length': data.length
-//   },
-//   body: data
-// };
-
-// fetch('https://graph.facebook.com/v15.0/105107965865939/messages', options)
-//   .then(res => {
-//     console.log(`statusCode: ${res.status}`);
-//     return res.json();
-//   })
-//   .then(data => {
-//     console.log(data);
-//   })
-//   .catch(err => {
-//     console.error(err);
-//   });
-
-
-
-
-
+//
 
 
 
